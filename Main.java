eb9311f2ab81b51706a228a2fb07cb2cc40a57aa
OlegLisitskiy <olegblisitskiy@mail.ru>
2024-08-03 23:09:39 +0300

    fird

--- Main.java
+++ Main.java
@@ -2,9 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hi");
         System.out.println("SECOND!");
-        System.out.println("1!");
-        System.out.println("2!");
-        System.out.println("3!");
+
     }
 }
 
