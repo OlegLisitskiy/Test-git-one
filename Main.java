7cfe33e0aa7482cad17f13ecfdee2803f1aeb297
OlegLisitskiy <olegblisitskiy@mail.ru>
2024-08-03 23:05:51 +0300

    third

--- Main.java
+++ Main.java
@@ -2,5 +2,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hi");
         System.out.println("SECOND!");
+        System.out.println("1!");
+        System.out.println("2!");
+        System.out.println("3!");
     }
 }
+
+
